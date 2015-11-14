994e76e247df220acb0d9fb78c8bff367dc20a51
Emmanuel Pericon <brianpericon.e92@gmail.com>
2015-11-14 11:03:36 -0300

    Adding steps of account_lockout.feature

--- features/step_definitions/account_lockout_step.rb
+++ features/step_definitions/account_lockout_step.rb
@@ -1,29 +1,24 @@
 Given(/^the login page$/) do
-  pending # express the regexp above with the code you wish you had
+	visit '/login'
 end
 
 Given(/^the account with email: "(.*?)" and password: "(.*?)"$/) do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
 end
 
-When(/^I insert the email "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When(/^I insert the email "(.*?)"$/) do |my_email|
+	fill_in('user[email]', with: my_email)
 end
 
-When(/^I insert the password "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When(/^I insert the password "(.*?)"$/) do |my_password|
+  fill_in('user[password]', with: my_password)
 end
 
 When(/^login$/) do
-  pending # express the regexp above with the code you wish you had
+	click_button('Login')
 end
 
-Then(/^I should see an error message "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^I should see an error message "(.*?)"\.$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^I should see an error message "(.*?)"$/) do |error_message|
+  page.should have_content(error_message)
 end
 
 Then(/^my account has to be blocked\.$/) do
