43897dd8de046f657446d833dd23bca67ae2c839
GianFF <gf.fioriello@gmail.com>
2015-11-28 18:48:36 -0300

    Adding more tests for #offers_finalized_by(reason)

--- spec/app/models/report_spec.rb
+++ spec/app/models/report_spec.rb
@@ -2,13 +2,24 @@ require 'spec_helper'
 
 describe Report do
 
-  #['Hiring open', 'Hiring in this site', 'Hiring in other site']
   describe '#offers_finalized_by(reason)' do
 
     it 'should return 0 when looking for "Hiring open"' do
-      Report.should_receive(:offers_finalized_by).with("Hiring open").and_return(0)
+      Report.should_receive(:offers_finalized_by).with('Hiring open').and_return(0)
 
       expect(Report.offers_finalized_by('Hiring open')).to eq 0
+    end
+
+    it 'should return 0 when looking for "Hiring in this site"' do
+      Report.should_receive(:offers_finalized_by).with('Hiring in this site').and_return(0)
+
+      expect(Report.offers_finalized_by('Hiring in this site')).to eq 0
+    end
+
+    it 'should return 0 when looking for "Hiring in other site"' do
+      Report.should_receive(:offers_finalized_by).with('Hiring in other site').and_return(0)
+
+      expect(Report.offers_finalized_by('Hiring in other site')).to eq 0
     end
   end
 
