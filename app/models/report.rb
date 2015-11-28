5d98d240d273c078018d14bb8a3a09efba719362
GianFF <gf.fioriello@gmail.com>
2015-11-28 18:43:24 -0300

    Refactoring method #self.offers_finalized_by(reason)

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,7 +1,8 @@
 class Report
 
-  def offers_finalized_by(reason)
-    0 
+  # This method looks in the data base for offers finalized by reason.
+  def self.offers_finalized_by(reason)
+    JobOffer.all(:reason => reason).size
   end
 
 end
--- spec/app/models/report_spec.rb
+++ spec/app/models/report_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe Report do
 
-  let(:report) { Report.new }
-
   #['Hiring open', 'Hiring in this site', 'Hiring in other site']
-  describe '#offers_finalized_by("Hiring open") ' do
-   
-    it 'should NO SE QUE IRA ACA' do
-      expect(report.offers_finalized_by("Hiring open")).to eq 0
+  describe '#offers_finalized_by(reason)' do
+
+    it 'should return 0 when looking for "Hiring open"' do
+      Report.should_receive(:offers_finalized_by).with("Hiring open").and_return(0)
+
+      expect(Report.offers_finalized_by('Hiring open')).to eq 0
     end
   end
 
